ead78b95b9dd1b675d10ca55fd2443c179717b58
parkstery <130825724+parkstery@users.noreply.github.com>
2026-03-04 05:16:28 +0900

    elevation right side width shrink 50%

--- App.tsx
+++ App.tsx
@@ -2503,7 +2503,7 @@ const App: React.FC = () => {
       {route && (
         <div className={`absolute bottom-[25px] z-[50] flex items-end justify-end transition-all duration-300 ease-out ${elevationExpanded ? 'right-4 w-[80%] max-w-[288px]' : 'right-16 w-12 h-12 group'}`}>
           <div className="bg-white/95 backdrop-blur-md rounded-[2rem] shadow-2xl flex items-center w-full border border-slate-200 p-1 overflow-hidden">
-            <button onClick={() => setElevationExpanded(!elevationExpanded)} title="Elevation Profile" className="flex-shrink-0 w-10 h-10 flex items-center justify-center text-slate-500 hover:text-blue-600 order-last">{elevationExpanded ? <ChevronRight size={20} /> : <AreaChartIcon size={20} />}</button>
+            <button onClick={() => setElevationExpanded(!elevationExpanded)} title="Elevation Profile" className="flex-shrink-0 w-5 h-10 flex items-center justify-center text-slate-500 hover:text-blue-600 order-last">{elevationExpanded ? <ChevronRight size={20} /> : <AreaChartIcon size={20} />}</button>
             {elevationExpanded && (
               <div className="flex-1 pl-3 pr-0 py-1 flex flex-col gap-1.5">
                 <div className="flex justify-between items-center">
