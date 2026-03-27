652b8a01b6ba3105bf91586bdfa805f5ed8cf327
parkstery <130825724+parkstery@users.noreply.github.com>
2026-03-27 15:03:12 +0900

    elevation profile move to right 16 -> 4

--- App.tsx
+++ App.tsx
@@ -3298,7 +3298,7 @@ const App: React.FC = () => {
       </div>
       {route && (
         <div
-          className={`absolute z-[1000] flex items-end justify-end transition-all duration-300 ease-out pointer-events-auto ${elevationExpanded ? 'right-4 w-[72%] max-w-[317px] [@media(orientation:landscape)]:w-[57%] [@media(orientation:landscape)]:max-w-[253px]' : 'right-16 w-[2.4rem] h-[2.4rem] group'}`}
+          className={`absolute z-[1000] flex items-end justify-end transition-all duration-300 ease-out pointer-events-auto ${elevationExpanded ? 'right-4 w-[72%] max-w-[317px] [@media(orientation:landscape)]:w-[57%] [@media(orientation:landscape)]:max-w-[253px]' : 'right-4 w-[2.4rem] h-[2.4rem] group'}`}
           style={{ bottom: `calc(25px + env(safe-area-inset-bottom, 0px) + ${bannerReservedPx}px)` }}
         >
           {/* <div className="bg-white/95 backdrop-blur-md rounded-[2rem] shadow-2xl flex items-center w-full border border-slate-200 p-1 overflow-hidden"> */}
