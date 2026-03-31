d38827e8cb8ebf383682d256280805cf9968c251
parkstery <130825724+parkstery@users.noreply.github.com>
2026-03-31 21:22:54 +0900

    mistake banner Id writing -EG

--- android/app/src/main/java/com/liveonsoft/cycle/MainActivity.java
+++ android/app/src/main/java/com/liveonsoft/cycle/MainActivity.java
@@ -24,8 +24,7 @@ import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends BridgeActivity {
-    private static final String TAG = "MainActivityAd";
-    private static final String ADMOB_BANNER_AD_UNIT_ID_RELEASE = "ca-app-pub-2386721030013396/2486360510";
+    private static final String TAG = "MainActivityAd";    
     private static final String ADMOB_BANNER_AD_UNIT_ID_TEST = "ca-app-pub-3940256099942544/6300978111";
 
     @Nullable
